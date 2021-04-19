a8cfd15c868f4a6b48e84c0f6a40f5d7ae9bef22
brunolune <brunocoupier@protonmail.com>
2021-04-19 15:18:27 +0200

    amelioration visuel menu

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,14 +5,14 @@ import "../styles/Navigation.css";
 const Navigation = () => {
     return (
 
-        <div >
+        <div className="navigation">
             <NavLink exact to="/"  activeClassName="nav-active">
                 Home
             </NavLink> 
-            <NavLink exact to="/EditProtocol" activeClassName="nav-active">
+            <NavLink exact to="/EditProtocol"   activeClassName="nav-active">
                 Edit
             </NavLink>
-            <NavLink exact to="/ReadProtocol" activeClassName="nav-active">
+            <NavLink exact to="/ReadProtocol"  activeClassName="nav-active">
                 Read
             </NavLink>
         </div>
